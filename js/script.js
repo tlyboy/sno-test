887b2181119a8ae3a5ea97fa76d9d27038dcabf0
Guany <1084072690@qq.com>
2022-06-11 08:55:07 +0800

    update

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 const username = document.querySelector('#username')
 const submit = document.querySelector('#submit')
+const result = document.querySelector('#result')
 
-if (username) {
+if (username && submit && result) {
   username.focus()
-}
 
-if (submit) {
+  const pattern = /^\d{15}[\u4e00-\u9fa5]{2,3}$/
+
+  const verify = () => {
+    if (pattern.test(username.value)) {
+      result.className = 'success'
+      result.innerHTML = '学号输入格式正确！'
+    } else {
+      result.className = 'error'
+      result.innerHTML = '学号输入格式错误！'
+    }
+  }
+
+  username.addEventListener('enter', verify)
+
   submit.addEventListener('click', e => {
     e.preventDefault()
-
-    const username = document.querySelector('#username').value
-    const pattern = /^\d{15}[\u4e00-\u9fa5]{2,3}$/
-    const result = document.querySelector('#result')
-
-    if (username && result) {
-      if (pattern.test(username)) {
-        result.className = 'success'
-        result.innerHTML = '学号输入格式正确！'
-      } else {
-        result.className = 'error'
-        result.innerHTML = '学号输入格式错误！'
-      }
-    }
+    verify()
   })
 }
